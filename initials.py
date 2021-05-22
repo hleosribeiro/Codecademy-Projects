7462ebf0459b369b1b1f5ee199a26336f895c83b
Hugo <hleosribeiro@gmail.com>
2021-05-22 20:18:31 -0300

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,24 @@
+#This program writes my initials as ASCII art
+#My initials are HLR
+
+#Defining letters
+#H
+h_type_1 = "H   H"
+h_type_2 = "HHHHH"
+#L
+l_type_1 = "L    "
+l_type_2 = "LLLLL"
+#R
+r_type_1 = "RRRR "
+r_type_2 = "R   R"
+r_type_3 = "R R  "
+r_type_4 = "R  R"
+
+#print letters
+print(h_type_1 + " " + l_type_1 + " " + r_type_1)
+print(h_type_1 + " " + l_type_1 + " " + r_type_2)
+print(h_type_1 + " " + l_type_1 + " " + r_type_2)
+print(h_type_2 + " " + l_type_1 + " " + r_type_1)
+print(h_type_1 + " " + l_type_1 + " " + r_type_3)
+print(h_type_1 + " " + l_type_1 + " " + r_type_4)
+print(h_type_1 + " " + l_type_2 + " " + r_type_2)
